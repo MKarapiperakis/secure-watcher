e0e113f2f607967593b57a07ac55344b2886e66e
Manos <mkarapiperakis21@gmail.com>
2025-02-11 15:05:12 +0200

    add multiple receivers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const chalk = require("chalk");
 const PORT = process.env.PORT || 3010;
-const environment = process.env.NODE_ENV || '';
+const environment = process.env.NODE_ENV || "";
 const serverInit = require("./server");
+const chokidar = require("chokidar");
+const notifyAdmin = require("./middlewares/notify-admin");
 
 const cors = require("cors");
 const mainErrorHandler = (err) => console.error(err);
@@ -14,15 +16,19 @@ serverInit().then((app) => {
 
   server.listen(PORT, () => {
     console.log(
-      "Up & running on http://localhost:" + chalk.blue.underline.bold(PORT) + " for environment: " + chalk.green.bold(environment)
+      "Up & running on http://localhost:" +
+        chalk.blue.underline.bold(PORT) +
+        " for environment: " +
+        chalk.green.bold(environment)
     );
-    console.log(
-      "Swagger UI is available on http://localhost:" +
-        chalk.blue.underline.bold(`${PORT}/data/api/doc`)
-    );
-    console.log(
-      "Metrics are available on http://localhost:" +
-        chalk.blue.underline.bold(`${PORT}/status`)
-    );
+  });
+
+  chokidar.watch("./src/video").on("all", (event, path) => {
+    if (event === "add" && process.env.NOTIFY_ADMIN == "true") {
+      console.log("here");
+
+      const receivers = process.env.EMAIL_RECEIVERS.split(",");
+      notifyAdmin(path, receivers);
+    }
   });
 });
---
+++ src/middlewares/notify-admin.js
@@ -0,0 +1,64 @@
+const nodemailer = require("nodemailer");
+const { BadRequestError } = require("../lib/errors");
+const fs = require("fs");
+module.exports = (filePath, receivers) => {
+  try {
+    main(filePath, receivers).then((response) => {
+      if (!!response) {
+        console.log("Mail sent successfully");
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error("Error deleting file:", err);
+          } else {
+            console.log("File deleted successfully:", filePath);
+          }
+        });
+      } else {
+        console.log("error notifying user");
+        throw new BadRequestError("Bad Request");
+      }
+    });
+  } catch (error) {
+    console.log(`error notifying user: ${error}`);
+  }
+
+  async function main(filePath, receivers) {
+    try {
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_SENDER,
+          pass: process.env.PASSWORD_SENDER,
+        },
+      });
+
+      let info = await transporter.sendMail({
+        from: `"Secure Watcher Automation Service" <${process.env.EMAIL_SENDER}>`,
+        to: process.env.EMAIL_SENDER,
+        bcc: receivers,
+        subject: `Camera Spotted Movement`,
+        html: `<p style="font-size: 24px; font-weight: bold; color: #d9534f; text-align: center;">⚠ Motion Detected! ⚠</p>
+          <p style="font-size: 18px; color: #333; text-align: center;">Your security camera has detected movement.</p>
+          <hr style="width: 50%; border: 1px solid #d9534f; margin: 20px auto;">
+          <div style="text-align: center;">
+              <p style="font-size: 16px; color: #555;">See the captured image below:</p>
+              <img src="cid:capture_image" alt="Captured Image" style="max-width: 100%; height: auto; border: 2px solid #d9534f;">
+          </div>`,
+        attachments: [
+          {
+            filename: filePath.split("/").pop(),
+            path: filePath,
+            cid: "capture_image",
+          },
+        ],
+      });
+
+      return info.messageId;
+    } catch (err) {
+      console.log("error sending email: ", err);
+      return null;
+    }
+  }
+};
