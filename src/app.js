63f062e3e2beb22ddb81207a0d65a912e133be6c
Manos <mkarapiperakis21@gmail.com>
2025-02-19 01:42:46 +0200

    add watermark folder

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ process.on("uncaughtException", mainErrorHandler);
 process.on("unhandledRejection", mainErrorHandler);
 
 const storeFolder = path.join(__dirname, "store");
+const waterMarkFolder = path.join(__dirname, "water-mark");
 
 if (!fs.existsSync(storeFolder)) {
   fs.mkdirSync(storeFolder, { recursive: true });
@@ -31,16 +32,20 @@ serverInit().then((app) => {
     );
   });
 
-  const watermarkPath = path.join(__dirname, "/assets/logo/logo_3.png");
+  const watermarkImage = path.join(__dirname, "/assets/logo/logo_3.png");
 
   chokidar
-    .watch("./src/camera/alarm/VF0410870SIJM/")
+    .watch("./src/camera/")
     .on("add", async (filePath) => {
       if (process.env.NOTIFY_ADMIN == "true") {
         try {
           const fileName = path.basename(filePath);
           const fileExtension = path.extname(fileName).toLowerCase();
           const storePath = path.join(storeFolder, `${Date.now()}_${fileName}`);
+          const waterMarkPath = path.join(
+            waterMarkFolder,
+            `${Date.now()}_${fileName}`
+          );
 
           if (![".jpg", ".jpeg", ".png"].includes(fileExtension)) {
             console.log(`Skipped non-image file: ${fileName}`);
@@ -53,32 +58,68 @@ serverInit().then((app) => {
             return;
           }
 
-          const image = sharp(filePath);
-          const watermark = await sharp(watermarkPath)
-            .resize(150) // Adjust size of watermark
-            .toBuffer();
+          setTimeout(() => {
+            fs.copyFile(filePath, storePath, async (err) => {
+              if (err) {
+                console.error("Error copying file:", err);
+              } else {
+                const image = sharp(storePath);
+                const watermark = await sharp(watermarkImage)
+                  .resize(150) // Adjust size of watermark
+                  .toBuffer();
 
-          // Process and save watermarked image
-          image
-            .composite([
-              {
-                input: watermark,
-                gravity: "southeast", // Position bottom-right
-              },
-            ])
-            .rotate()
-            .resize(600)
-            .jpeg({ mozjpeg: true })
-            .toFile(storePath) // Save directly to file instead of using buffer
-            .then(async () => {
-              console.log("Watermarked image saved:", storePath);
+                // Process and save watermarked image
+                image
+                  .composite([
+                    {
+                      input: watermark,
+                      gravity: "southeast", // Position bottom-right
+                    },
+                  ])
+                  .rotate()
+                  .resize(600)
+                  .jpeg({ mozjpeg: true })
+                  .toFile(waterMarkPath)
+                  .then(async () => {
+                    console.log("Watermarked image saved:", storePath);
 
-              const receivers = process.env.EMAIL_RECEIVERS?.split(",") || [];
-              // notifyAdmin(storePath, receivers);
-            })
-            .catch((err) => {
-              console.error("Error processing image:", err);
+                    fs.unlink(filePath, (err) => {
+                      if (err) {
+                        console.error("Error deleting file:", err);
+                      } else {
+                        console.log("File deleted successfully:", filePath);
+
+                        const parentFolder = path.dirname(filePath);
+
+                        fs.readdir(parentFolder, (err, files) => {
+                          if (err) {
+                            console.error("Error reading folder:", err);
+                          } else if (files.length === 0) {
+                            // If empty, delete the parent folder
+                            fs.rmdir(parentFolder, (err) => {
+                              if (err) {
+                                console.error("Error deleting folder:", err);
+                              } else {
+                                console.log(
+                                  "Parent folder deleted:",
+                                  parentFolder
+                                );
+                              }
+                            });
+                          }
+                        });
+                      }
+                    });
+                    const receivers =
+                      process.env.EMAIL_RECEIVERS?.split(",") || [];
+                      notifyAdmin(waterMarkPath, receivers);
+                  })
+                  .catch((err) => {
+                    console.error("Error processing image:", err);
+                  });
+              }
             });
+          }, 3000);
         } catch (err) {
           console.error("Error processing file:", err);
         }
